e8ee316d62d3c46c41ea3254acb4beabe894ccd7
Manuel Gainza <gainza2014@gmail.com>
2021-11-02 18:42:28 -0300

    Se actualizo el controlador de los errores

--- Turistearg/src/main/java/com/turistearg/Controladores/ErrorController.java
+++ Turistearg/src/main/java/com/turistearg/Controladores/ErrorController.java
@@ -1,16 +1,63 @@
 
 package com.turistearg.Controladores;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping("/")
 public class ErrorController {
-     @GetMapping("")
-    public String index(){
-        return "index";
+     @RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+
+        ModelAndView errorPage = new ModelAndView("error");
+        String errorMsg = "";
+
+        int httpErrorCode = getErrorCode(httpRequest);
+
+        switch (httpErrorCode) {
+
+            case 400: {
+                errorMsg = "El recurso solicitado no existe";
+                break;
+            }
+
+            case 403: {
+                errorMsg = "No tiene permisos para acceder al recurso";
+                break;
+            }
+
+            case 401: {
+                errorMsg = "No se encuentra autorizado";
+                break;
+            }
+
+            case 404: {
+                errorMsg = "El recurso solicitado no fue encontrado";
+                break;
+            }
+            
+            case 500: {
+                errorMsg = "Error Interno del Servidor ";
+                break;
+            }
+
+        }
+
+        errorPage.addObject("codigo", httpErrorCode);
+        errorPage.addObject("errorMsg", errorMsg);
+        return errorPage;
+
+    }
+
+    
+    /* Obtengo el codigo de la solicitud http*/
+    
+    private int getErrorCode(HttpServletRequest httpRequest) {
+        return (Integer) httpRequest
+                .getAttribute("javax.servlet.error.status_code");
     }
 }
