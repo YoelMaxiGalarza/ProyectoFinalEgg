23ce68555d767989d0bf1cca75fb8490f7417909
juliancantons <cantonsjulian@gmail.com>
2021-11-03 20:13:22 -0300

    Modificacion Querys

--- Turistearg/src/main/java/com/turistearg/Repositorios/LugarRepositorio.java
+++ Turistearg/src/main/java/com/turistearg/Repositorios/LugarRepositorio.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface LugaresRepositorio extends JpaRepository <Lugares, String> {
+public interface LugarRepositorio extends JpaRepository <Lugares, String> {
     
 }
