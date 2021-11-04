db514351564e5ff59a1930d5ef57c8042d5895fc
Yoel Galarza <56613453+YoelMaxiGalarza@users.noreply.github.com>
2021-11-04 18:42:05 -0300

    Update de metodos servicios
    Puliendo unas cosas

--- Turistearg/src/main/java/com/turistearg/Repositorios/LugarRepositorio.java
+++ Turistearg/src/main/java/com/turistearg/Repositorios/LugarRepositorio.java
@@ -1,6 +1,5 @@
 package com.turistearg.Repositorios;
 
-import com.turistearg.Entidades.Lugar;
 import com.turistearg.Enumeraciones.Lugares;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
