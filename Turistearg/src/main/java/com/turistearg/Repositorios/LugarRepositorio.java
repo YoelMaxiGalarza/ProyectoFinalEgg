ebf6782c2e6c201ff943b39a387906c9e06127d2
Yoel Galarza <56613453+YoelMaxiGalarza@users.noreply.github.com>
2021-11-04 19:04:59 -0300

    Puliendo unas cosas
    Cambios en los controladores y en los metodos

--- Turistearg/src/main/java/com/turistearg/Repositorios/LugarRepositorio.java
+++ Turistearg/src/main/java/com/turistearg/Repositorios/LugarRepositorio.java
@@ -1,11 +1,12 @@
 package com.turistearg.Repositorios;
 
-import com.turistearg.Enumeraciones.Lugares;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.turistearg.Entidades.Lugar;
+
 
 @Repository
-public interface LugarRepositorio extends JpaRepository <Lugares, String> {
+public interface LugarRepositorio extends JpaRepository <Lugar, String> {
   
 }
