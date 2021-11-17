1007b753e6850e7e1c054df489f1f4e94e2d0c54
Manuel Gainza <gainza2014@gmail.com>
2021-11-17 20:31:58 -0300

    Modificaciones en PublicacionController
    Se realizaron las modificaciones para poder crear una publicacion

--- Turistearg/src/main/java/com/turistearg/Controladores/PublicacionController.java
+++ Turistearg/src/main/java/com/turistearg/Controladores/PublicacionController.java
@@ -1,17 +1,25 @@
 
 package com.turistearg.Controladores;
 
+import com.turistearg.Entidades.Categoria;
 import com.turistearg.Entidades.Publicacion;
+import com.turistearg.Entidades.Usuario;
 import com.turistearg.Excepciones.ErrorServicio;
 import com.turistearg.Repositorios.PublicacionRepositorio;
+import com.turistearg.Servicios.CategoriaServicio;
 import com.turistearg.Servicios.PublicacionServicio;
+import com.turistearg.Servicios.UsuarioServicio;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/publicacion")
@@ -20,6 +28,12 @@ public class PublicacionController {
     
     @Autowired
     PublicacionServicio publicacionServicio;
+    
+    @Autowired
+    UsuarioServicio usuarioServicio;
+    
+    @Autowired
+    CategoriaServicio categoriaServicio;
     
     @GetMapping("/publicaciones")
     public String publicaciones(ModelMap model, @RequestParam("id") String idCategoria) throws ErrorServicio {
@@ -39,5 +53,65 @@ public class PublicacionController {
         return "publicaciones";
 
     }
+    
+    @PreAuthorize(("hasAnyRole('ROLE_USUARIO_REGISTRADO')"))
+    @GetMapping("/crear")
+    public String displayCrearPublicar(ModelMap model, HttpSession session, @RequestParam String idCategoria,
+            @RequestParam String idUsuario){
+        
+          Usuario login = (Usuario) session.getAttribute("usuariosession");
+
+        if (login == null || !login.getId().equals(idUsuario)) {
+            return "redirect:/";
+        }
+        
+        
+        try {
+            
+            Usuario usuario = usuarioServicio.buscarPorId(idUsuario);
+                     
+            model.put("usuario", usuario);       
+            
+        } catch (ErrorServicio e) {
+            model.put("error", e.getMessage());
+        }
+        
+        return "crear-publicacion";
+        
+    }
+    
+    
+    @PostMapping("/publicar")
+    public String publicar(ModelMap model, @RequestParam String idUsuario, @RequestParam String idCategoria,
+            @RequestParam(required = false) String descripcion, @RequestParam MultipartFile foto){
+        
+        try {
+            
+          publicacionServicio.crear(foto, idUsuario, descripcion, idCategoria);
+          
+          
+        } catch (ErrorServicio e) {
+            model.put("error", e.getMessage());
+          
+            return "redirect:/crear";
+        }
+
+        return "redirect:/publicaciones";
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
     
 }
