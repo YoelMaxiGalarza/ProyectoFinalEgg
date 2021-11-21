04981c8432b2109fe641bf1f994b3db8e86d3928
Yoel Galarza <yoelgalarza@gmail.com>
2021-11-21 18:31:28 -0300

    Cambios en vistas de categoria y cosas varias
    *Modificaciones en categoria para intentar subir una publicacion desde la vista de las publicaciones de la categoria
    *Borrado de algunos imports innecesarios
    *Colocacion del link en el la vista publicaciones para crear la publicacion de la categoria

--- Turistearg/src/main/java/com/turistearg/Controladores/CategoriaController.java
+++ Turistearg/src/main/java/com/turistearg/Controladores/CategoriaController.java
@@ -3,20 +3,16 @@ package com.turistearg.Controladores;
 import com.turistearg.Entidades.Categoria;
 import com.turistearg.Entidades.Lugar;
 import com.turistearg.Excepciones.ErrorServicio;
-import com.turistearg.Repositorios.CategoriaRepositorio;
-import com.turistearg.Repositorios.LugarRepositorio;
 import com.turistearg.Servicios.CategoriaServicio;
 import com.turistearg.Servicios.LugarServicio;
+import com.turistearg.Servicios.PublicacionServicio;
 
 import java.util.List;
 
-import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CategoriaController {
@@ -27,6 +23,7 @@ public class CategoriaController {
     
     @Autowired
     private LugarServicio lugarServicio;
+    
     
     @GetMapping("/categorias")
     public String categorias(ModelMap model, String idLugar) throws ErrorServicio {
@@ -46,5 +43,6 @@ public class CategoriaController {
         return "categorias";
 
     }
+    
     
 }
